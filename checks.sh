abbf3c6805417a28de126a33ac99674fce727fb9
soegaeni <szogyenyid@gmail.com>
2023-05-06 11:50:35 +0200

    Added pest to checks.sh

--- checks.sh
+++ checks.sh
@@ -2,4 +2,5 @@
 ./vendor/bin/phpmd src text codesize,unusedcode,naming,design,cleancode      
 ./vendor/bin/phpcs -ns --report=summary --extensions=php --standard=PSR12 src
 ./vendor/bin/phpcs src --standard=Squiz --sniffs=Squiz.Commenting.FunctionComment,Squiz.Commenting.FunctionCommentThrowTag,Squiz.Commenting.ClassComment,Squiz.Commenting.VariableComment
+./vendor/bin/pest --coverage
 ./vendor/bin/phpcs -p src --standard=PHPCompatibility --runtime-set testVersion 7.4-
