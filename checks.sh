0f59abbfb26c2f7e9fee77b4fb049199fbfcaf26
soegaeni <szogyenyid@gmail.com>
2023-05-14 12:10:59 +0200

    Move all the checks to github actions

--- checks.sh
+++
@@ -1,7 +0,0 @@
-composer update
-./vendor/bin/phpstan analyse --memory-limit=1G --level=9 src
-./vendor/bin/phpmd src text codesize,unusedcode,naming,design,cleancode      
-./vendor/bin/phpcs -ns --report=summary --extensions=php --standard=PSR12 src
-./vendor/bin/phpcs src --standard=Squiz --sniffs=Squiz.Commenting.FunctionComment,Squiz.Commenting.FunctionCommentThrowTag,Squiz.Commenting.ClassComment,Squiz.Commenting.VariableComment
-./vendor/bin/pest --coverage --min=95
-./vendor/bin/phpcs -p src --standard=PHPCompatibility --runtime-set testVersion 7.4-
